a7cb2effdc4a75c3541d7dc2e009803ec3001244
Ekta K <diego.developers@robosftin.com>
2017-03-09 11:22:38 +0530

    Shifted working copies to local folder

--- try_out/app/helpers/orders_helper.rb
+++ try_out/app/helpers/orders_helper.rb
@@ -1,6 +1,20 @@
 module OrdersHelper
-    def get_grocer_name order
-        grocer = Grocer.find(order.grocer_id)
-        grocer.name
+    def get_name order
+        if ( current_user.user_type == "Grocer" )
+            client = Client.find(order.client_id)
+            client.name
+        else
+            grocer = Grocer.find(order.grocer_id)
+            grocer.name
+        end
+    end
+
+    def delete_order order
+         order.destroy
+    end
+
+    def get_order_status_name order
+        order_status = OrderStatus.find(order.order_status_id)
+        order_status.status
     end
 end
